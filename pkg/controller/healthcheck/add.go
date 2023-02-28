34e67c983de2fd5bfe287fbd27436e6f033a0d31
Shafeeque E S <shafeeque.e.s@sap.com>
2023-02-28 10:23:52 +0530

    Remove MR health check for `ShootSystemComponentsHealthy` condition
    Gardener now checks all the ManagedResources in the shoot controlplane which does not specify `spec.class`, so these MRs are already covered.

--- pkg/controller/healthcheck/add.go
+++ pkg/controller/healthcheck/add.go
@@ -18,11 +18,9 @@ import (
 	"time"
 
 	healthcheckconfig "github.com/gardener/gardener/extensions/pkg/apis/config"
-	genericcontrolplaneactuator "github.com/gardener/gardener/extensions/pkg/controller/controlplane/genericactuator"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/general"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/worker"
-	genericworkeractuator "github.com/gardener/gardener/extensions/pkg/controller/worker/genericactuator"
 	extensionspredicate "github.com/gardener/gardener/extensions/pkg/predicate"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
@@ -50,7 +48,7 @@ var (
 )
 
 // RegisterHealthChecks registers health checks for each extension resource
-// HealthChecks are grouped by extension (e.g worker), extension.type (e.g vsphere) and  Health Check Type (e.g SystemComponentsHealthy)
+// HealthChecks are grouped by extension (e.g worker), extension.type (e.g vsphere) and  Health Check Type (e.g ShootControlPlaneHealthy)
 func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) error {
 	if err := healthcheck.DefaultRegistration(
 		vsphere.Type,
@@ -65,14 +63,6 @@ func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) 
 				ConditionType: string(gardencorev1beta1.ShootControlPlaneHealthy),
 				HealthCheck:   general.NewSeedDeploymentHealthChecker(vsphere.CloudControllerManagerName),
 			},
-			{
-				ConditionType: string(gardencorev1beta1.ShootSystemComponentsHealthy),
-				HealthCheck:   general.CheckManagedResource(genericcontrolplaneactuator.ControlPlaneShootChartResourceName),
-			},
-			{
-				ConditionType: string(gardencorev1beta1.ShootSystemComponentsHealthy),
-				HealthCheck:   general.CheckManagedResource(genericcontrolplaneactuator.StorageClassesChartResourceName),
-			},
 		}); err != nil {
 		return err
 	}
@@ -86,10 +76,6 @@ func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) 
 		opts,
 		nil,
 		[]healthcheck.ConditionTypeToHealthCheck{
-			{
-				ConditionType: string(gardencorev1beta1.ShootSystemComponentsHealthy),
-				HealthCheck:   general.CheckManagedResource(genericworkeractuator.McmShootResourceName),
-			},
 			{
 				ConditionType: string(gardencorev1beta1.ShootControlPlaneHealthy),
 				HealthCheck:   general.NewSeedDeploymentHealthChecker(vsphere.MachineControllerManagerName),
